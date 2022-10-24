03fd1ab9ba5ac8f74d162e4d1f4d4dac1e5f3641
Adamauly <115804268+adamauly@users.noreply.github.com>
2022-10-24 03:37:02 +0200

    Added a function to check winner of a round
    The function singleRound returns the winner of that round

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,10 +1,13 @@
+let playerWin = 0;
+let computerWin = 0;
+
 function getComputerChoice() {
     if (getRandomInt() == 0) {
-        return "Rock"
+        return "rock"
     } else if (getRandomInt() == 1) {
-        return "Paper"
+        return "paper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
@@ -14,9 +17,42 @@ function getRandomInt() {
 
 function playerSelection() {
     let weapon = prompt("Please enter 'Rock', 'Paper', or 'Scissors': ").toLowerCase;
-    return weapon;
 }
 
+function singleRound(getComputerChoice, playerSelection) {
+    if (getComputerChoice() == "rock" && playerSelection == "scissors") {
+        ++computerWin
+        return "Lose"
+    } else if (getComputerChoice() == "rock" && playerSelection == "rock") {
+        ++tie
+        return "Tied"
+    } else if (getComputerChoice() == "rock" && playerSelection == "paper") {
+        ++playerWin
+        return "Won"
+    } else if (getComputerChoice() == "paper" && playerSelection == "rock") {
+        ++computerWin
+        return "Lose"
+    } else if (getComputerChoice() == "paper" && playerSelection == "paper") {
+        ++tie
+        return "tie"
+    } else if (getComputerChoice() == "paper" && playerSelection == "scissors") {
+        ++playerWin
+        return "Won"
+    } else if (getComputerChoice() == "scissors" && playerSelection == "paper") {
+        ++computerWin
+        return "Lose"
+    } else if (getComputerChoice() == "scissors" && playerSelection == "scissors") {
+        ++tie
+        return "Tied"
+    } else if (getComputerChoice() == "scissors" && playerSelection == "rock") {
+        ++playerWin
+        return "Won"
+    }
 
+
+
+if (singleRound() != "Tied") {
+
+}  
 
 console.log(getComputerChoice())
