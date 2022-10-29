175b886a3de5a8b8297c0bb8347c3c42dda06134
Adamauly <115804268+adamauly@users.noreply.github.com>
2022-10-29 12:55:22 +0200

    Added 3 buttons and event listeners to each of them.
    Disabled the actual game by commenting out game().

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,6 +1,12 @@
 let playerWin = 0;
 let computerWin = 0;
 let tie = 0;
+const buttons = document.querySelectorAll("button")
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        alert(btn.id);
+    });
+});
 
 function getComputerChoice() {
     if (getRandomInt() == 0) {
@@ -26,61 +32,83 @@ function singleRound(computerChoice, playerChoice) {
     if (computerChoice == "rock" && playerChoice== "scissors") {
         ++computerWin
         return "Lose"
-    } else if (computerChoice == "rock" && playerChoice== "rock") {
+    } else if (computerChoice == "rock" && playerChoice == "rock") {
         ++tie
         return "Tied"
-    } else if (computerChoice == "rock" && playerChoice== "paper") {
+    } else if (computerChoice == "rock" && playerChoice == "paper") {
         ++playerWin
         return "Won"
-    } else if (computerChoice == "paper" && playerChoice== "rock") {
+    } else if (computerChoice == "paper" && playerChoice == "rock") {
         ++computerWin
         return "Lose"
-    } else if (computerChoice == "paper" && playerChoice== "paper") {
+    } else if (computerChoice == "paper" && playerChoice == "paper") {
         ++tie
         return "Tied"
-    } else if (computerChoice == "paper" && playerChoice== "scissors") {
+    } else if (computerChoice == "paper" && playerChoice == "scissors") {
         ++playerWin
         return "Won"
-    } else if (computerChoice == "scissors" && playerChoice== "paper") {
+    } else if (computerChoice == "scissors" && playerChoice == "paper") {
         ++computerWin
         return "Lose"
-    } else if (computerChoice == "scissors" && playerChoice== "scissors") {
+    } else if (computerChoice == "scissors" && playerChoice == "scissors") {
         ++tie
         return "Tied"
-    } else if (computerChoice == "scissors" && playerChoice== "rock") {
+    } else if (computerChoice == "scissors" && playerChoice == "rock") {
         ++playerWin
         return "Won"
     }
 }
 
 function capitalizeFirstLetter(text) {
-    console.log("Text parameter is: " + text)
     firstLetter = text[0].toUpperCase();
     return (firstLetter + text.slice(1));
 }
 
-function game() {
-    while ((playerWin || computerWin) < 5) {
-        let computerChoice = getComputerChoice();
-        let playerChoice = playerSelection();
+// function game() {
+//     while ((playerWin || computerWin) < 5) {
+//         let computerChoice = getComputerChoice();
+//         let playerChoice = playerSelection();
     
-        roundResult = singleRound(computerChoice, playerChoice);
+//         roundResult = singleRound(computerChoice, playerChoice);
     
-        if (roundResult != "Tied") {
-            alert("You " + roundResult + "! " + capitalizeFirstLetter(playerChoice) + " beats " + capitalizeFirstLetter(computerChoice) + ".")
-        } else {
-            alert("You" + roundResult + "! Try again.")
-        }
+//         if (roundResult != "Tied") {
+//             alert("You " + roundResult + "! " + capitalizeFirstLetter(playerChoice) + " beats " + capitalizeFirstLetter(computerChoice) + ".")
+//         } else {
+//             alert("You " + roundResult + "! Try again.")
+//         }
     
-        console.log("Computer wins: " + computerWin)
-        console.log("Player wins: " + playerWin)
-    }
+//         console.log("Computer wins: " + computerWin)
+//         console.log("Player wins: " + playerWin)
+//     }
     
-    if (playerWin == 5) {
-        alert("Congratulations! You won!\n Reload the page to play again!")
-    } else {
-        alert("You lost!\n Reload the page and try again!")
-    }
-}
+//     if (playerWin == 5) {
+//         alert("Congratulations! You won!\n Reload the page to play again!")
+//     } else {
+//         alert("You lost!\n Reload the page and try again!")
+//     }
+// }
+
+// function game() {
+//     let computerChoice = getComputerChoice();
+//     let playerChoice = playerSelection();
+
+//     roundResult = singleRound(computerChoice, playerChoice);
+
+//     if (roundResult != "Tied") {
+//         alert("You " + roundResult + "! " + capitalizeFirstLetter(playerChoice) + " beats " + capitalizeFirstLetter(computerChoice) + ".")
+//     } else {
+//         alert("You " + roundResult + "! Try again.")
+//     }
+
+//     console.log("Computer wins: " + computerWin)
+//     console.log("Player wins: " + playerWin)
+// }
+
+// if (playerWin == 5) {
+//     alert("Congratulations! You won!\n Reload the page to play again!")
+// } else {
+//     alert("You lost!\n Reload the page and try again!")
+// }
+
 
 game();
